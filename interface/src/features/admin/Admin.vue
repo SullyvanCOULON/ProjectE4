a56d3ac6f848ae5e5d622c26516a05bc0b7964e6
Sullyvan Coulon <sullyvancoulon@Sullyvans-MacBook-Air.local>
2025-04-25 10:30:28 +0200

    Interface duplacated correction

--- interface/src/features/admin/Admin.vue
+++ interface/src/features/admin/Admin.vue
@@ -1,12 +1,10 @@
 <script setup lang="ts">
-import DocsForm from './components/DocsForm.vue';
 
 const props = defineProps<{
 }>();
 </script>
 <template>
   <div class="admin-container d-flex flex-row justify-content-center align-items-center">
-    <DocsForm/>
   </div>
 </template>
 <style scoped lang="scss">
